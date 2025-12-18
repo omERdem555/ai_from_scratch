263a33af31987516655ecd57820c3511ea175998
omERdem555 <omererdem555@outlook.com>
2025-12-18 21:44:35 +0300

    Sigmoid Model düzenlendi

--- Models/AktivasyonFonksiyonluTekNoron/Program.cs
+++ Models/AktivasyonFonksiyonluTekNoron/Program.cs
@@ -11,7 +11,7 @@ internal class Program
         double[] xs = { 0, 1, 2, 3, 4, 5 };
         double[] ys = { 0, 0, 0, 1, 1, 1 };
 
-        for (int epoch = 0; epoch < 10; epoch++)
+        for (int epoch = 0; epoch < 1000; epoch++)
         {
             for (int i = 0; i < 15; i++)
             {
@@ -21,25 +21,34 @@ internal class Program
             for (int i = 0; i < xs.Length; i++)
             {
                 Console.Write(i + " ");
-                Train(model, xs[i], ys[i], 0.01);
+                Train(model, xs[i], ys[i], 0.1);
                 Console.WriteLine($"W: {model.W}, B: {model.B}");
                 Console.WriteLine(new string('-', 10));
             }
             Console.WriteLine("\n");
         }
         Console.WriteLine($"Final W: {model.W}, Final B: {model.B}");
+
+        Console.WriteLine("\n");
+        Console.WriteLine("\n");
+        Console.WriteLine("\n");
+
+        for (int i = 0; i < xs.Length; i++)
+        {
+            double pred = model.Predict(xs[i]);
+            Console.WriteLine($"x={xs[i]} -> {pred:F3}");
+        }
+
     }
 
 
     static void Train(SigmoidModel model, double x, double y, double learningRate)
     {
         double prediction = model.Predict(x);
-        Console.WriteLine($"Prediction: {prediction}");
-        double error = prediction - y;
-        Console.WriteLine($"Error: {error}");
-        // türevler
-        double dZ = error * prediction * (1 - prediction); // Sigmoid türevi
-        model.W -= learningRate * dZ * x;
-        model.B -= learningRate * dZ;
+        double error = prediction - y;   // BCE + Sigmoid için bu yeterli
+
+        model.W -= learningRate * error * x;
+        model.B -= learningRate * error;
     }
+
 }
