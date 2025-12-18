ababe6d34c6220a9c9721a2bb5e93fe7ba713a8f
omERdem555 <omererdem555@outlook.com>
2025-12-19 00:30:00 +0300

    Çok girişli tek nöron

---
+++ MultipleInputs/SingleNeuronMultipleInputs/LoanApprovalDecision/LoanApprovalDecision/NeuronModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kredi_Onay_Kararı
+{
+    public class NeuronModel
+    {
+        public double[] W; // ağırlıklar
+        public double B;   // bias
+
+        public NeuronModel(int inputSize)
+        {
+            W = new double[inputSize];
+            B = 0;
+        }
+
+        public double Predict(double[] x)
+        {
+            double z = B;
+            for (int i = 0; i < x.Length; i++)
+                z += W[i] * x[i];
+
+            return Sigmoid(z);
+        }
+
+        private double Sigmoid(double z)
+        {
+            return 1.0 / (1.0 + Math.Exp(-z));
+        }
+    }
+
+}
--- MultipleInputs/SingleNeuronMultipleInputs/LoanApprovalDecision/LoanApprovalDecision/Program.cs
+++ MultipleInputs/SingleNeuronMultipleInputs/LoanApprovalDecision/LoanApprovalDecision/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Kredi_Onay_Kararı;
+
+internal class Program
+{
+    static void Main()
+    {
+        // 3 özellikli basit bir nöron modeli
+        var model = new NeuronModel(3);
+
+
+        // Eğitim verisi: [Gelir, Kredi Skoru, Borç/ Gelir Oranı] -> [Onay (1) / Reddet (0)]
+        double[][] X =
+        {
+        new double[] {0.9, 0.1, 0.8},
+        new double[] {0.8, 0.2, 0.7},
+        new double[] {0.7, 0.3, 0.6},
+        new double[] {0.4, 0.6, 0.4},
+        new double[] {0.3, 0.7, 0.3},
+        new double[] {0.2, 0.8, 0.2}
+    };
+
+        double[] Y = { 1, 1, 1, 0, 0, 0 };// Etiketler
+
+        // Eğitim döngüsü
+        for (int epoch = 0; epoch < 1000; epoch++)
+        {
+            for (int i = 0; i < X.Length; i++)
+                Train(model, X[i], Y[i], 0.1);
+        }
+
+        Console.WriteLine("Test:");
+        foreach (var x in X)
+            Console.WriteLine(model.Predict(x).ToString("F3"));
+    }
+
+
+
+    /// <summary>
+    /// Train (BCE + Sigmoid, sade hali)
+    /// </summary>
+    /// <param name="model"></param>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <param name="learningRate"></param>
+    static void Train(NeuronModel model, double[] x, double y, double learningRate)
+    {
+        double yPred = model.Predict(x);
+
+        // BCE + Sigmoid türevi sadeleşir
+        double error = yPred - y;
+
+        for (int i = 0; i < x.Length; i++)
+            model.W[i] -= learningRate * error * x[i];
+
+        model.B -= learningRate * error;
+    }
+
+}
