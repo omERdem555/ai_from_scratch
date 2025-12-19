ef61fe32b201a1aae1c151bdf2221b4dd2d848df
omERdem555 <omererdem555@outlook.com>
2025-12-19 20:53:15 +0300

    DenseLayer Genel Model Sınıfı (N giriş / N nöron)

---
+++ MultifoliateNets/DenseLayer/Neuron.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DenseLayer
+{
+    public class Neuron
+    {
+        public int InputSize;
+        public int NeuronCount;
+
+        public double[,] W; // [neuron, input]
+        public double[] B;  // [neuron]
+
+        public Neuron(int inputSize, int neuronCount)
+        {
+            InputSize = inputSize;
+            NeuronCount = neuronCount;
+
+            W = new double[neuronCount, inputSize];
+            B = new double[neuronCount];
+
+            // Küçük rastgele başlatma
+            var rnd = new Random();
+            for (int n = 0; n < neuronCount; n++)
+                for (int i = 0; i < inputSize; i++)
+                    W[n, i] = rnd.NextDouble() * 0.1;
+        }
+
+        public double[] Predict(double[] x)
+        {
+            double[] outputs = new double[NeuronCount];
+
+            for (int n = 0; n < NeuronCount; n++)
+            {
+                double z = B[n];
+                for (int i = 0; i < InputSize; i++)
+                    z += W[n, i] * x[i];
+
+                outputs[n] = Sigmoid(z);
+            }
+
+            return outputs;
+        }
+
+        private double Sigmoid(double z)
+        {
+            return 1.0 / (1.0 + Math.Exp(-z));
+        }
+    }
+
+}
---
+++ MultifoliateNets/DenseLayer/Program.cs
@@ -0,0 +1,35 @@
+﻿using DenseLayer;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var layer = new Neuron(inputSize: 3, neuronCount: 3);
+
+        double[] x = { 0.8, 0.2, 0.9 };
+        double[] y = { 1, 0, 1 };
+
+        for (int epoch = 0; epoch < 1000; epoch++)
+        {
+            Train(layer, x, y, 0.1);
+        }
+
+        double[] result = layer.Predict(x);
+    }
+
+
+    static void Train(Neuron layer, double[] x, double[] y, double learningRate)
+    {
+        double[] yPred = layer.Predict(x);
+
+        for (int n = 0; n < layer.NeuronCount; n++)
+        {
+            double error = yPred[n] - y[n];
+
+            for (int i = 0; i < layer.InputSize; i++)
+                layer.W[n, i] -= learningRate * error * x[i];
+
+            layer.B[n] -= learningRate * error;
+        }
+    }
+
+}
